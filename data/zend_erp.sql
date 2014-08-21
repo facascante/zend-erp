4ebb8839d90586697405c882a17011c3c600c5c0
facascante <cfajr@rocketmail.com>
2014-08-21 13:43:18 +0800

    sync

--- data/zend_erp.sql
+++ data/zend_erp.sql
@@ -1,59 +1,57 @@
--- MySQL dump 10.13  Distrib 5.5.25, for osx10.6 (i386)
---
--- Host: localhost    Database: zend_erp
--- ------------------------------------------------------
--- Server version	5.5.25
+/*
+SQLyog Trial v12.0 (64 bit)
+MySQL - 5.6.16 : Database - zend_erp
+*********************************************************************
+*/
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
+
+/*!40101 SET SQL_MODE=''*/;
+
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`zend_erp` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
---
--- Table structure for table `tblcustomer`
---
+USE `zend_erp`;
+
+
+/*Table structure for table `tblcustomer` */
 
 DROP TABLE IF EXISTS `tblcustomer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `tblcustomer` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `custumer_category` varchar(50) NOT NULL,
-  `customer_type` varchar(50) NOT NULL,
-  `customer_name` varchar(50) NOT NULL,
-  `customer_consignee` varchar(50) NOT NULL,
-  `customer_branch` text,
-  `customer_trade_name` varchar(50) NOT NULL,
-  `customer_phone` varchar(50) NOT NULL,
-  `customer_email` varchar(50) NOT NULL,
-  `status` varchar(50) NOT NULL,
-  `customer_tin` int(11) NOT NULL,
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`category_id` int(10) NOT NULL,
+	`type_id` int(10) NOT NULL,
+	`owner_name` varchar(50) NOT NULL,
+	`company_name` varchar(50) NOT NULL,
+	`consignee` varchar(20) NOT NULL,
+	`branch` varchar(50) NOT NULL,
+	`franchise` varchar(50) NOT NULL,
+	`phone` varchar(15) NOT NULL,
+	`trade_name` varchar(50) NOT NULL,
+	`tin` int(11) NOT NULL,  
+  	`website` varchar(50) NOT NULL,
+	`email` varchar(50) NOT NULL,
+	`credit_allow_id` int(11) NOT NULL,
+	`payment_term_id` int(11) NOT NULL,
+	`price_type_id` int(11) NOT NULL,
+	`se_id` int(11) NOT NULL,
+	`status` varchar(50) NOT NULL,
+  
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `tblcustomer`
---
+/*Data for the table `tblproduct` */
 
-LOCK TABLES `tblcustomer` WRITE;
-/*!40000 ALTER TABLE `tblcustomer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tblcustomer` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `tblproduct`
---
+/*Table structure for table `tblproduct` */
 
 DROP TABLE IF EXISTS `tblproduct`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `tblproduct` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `code` int(11) unsigned NOT NULL,
@@ -68,80 +66,27 @@ CREATE TABLE `tblproduct` (
   `supplier_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `tblproduct`
---
+/*Data for the table `tblproduct` */
 
-LOCK TABLES `tblproduct` WRITE;
-/*!40000 ALTER TABLE `tblproduct` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tblproduct` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tblproducts`
---
-
-DROP TABLE IF EXISTS `tblproducts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tblproducts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `code` int(11) unsigned NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `brand` varchar(50) NOT NULL,
-  `model` varchar(50) NOT NULL,
-  `description` text,
-  `uom` varchar(50) NOT NULL,
-  `size` varchar(50) NOT NULL,
-  `weight` varchar(50) NOT NULL,
-  `status` varchar(50) NOT NULL,
-  `supplier` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tblproducts`
---
-
-LOCK TABLES `tblproducts` WRITE;
-/*!40000 ALTER TABLE `tblproducts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tblproducts` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tblrole`
---
+/*Table structure for table `tblrole` */
 
 DROP TABLE IF EXISTS `tblrole`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `tblrole` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `tblrole`
---
+/*Data for the table `tblrole` */
 
-LOCK TABLES `tblrole` WRITE;
-/*!40000 ALTER TABLE `tblrole` DISABLE KEYS */;
-INSERT INTO `tblrole` VALUES (1,'Developer'),(2,'Administrator');
-/*!40000 ALTER TABLE `tblrole` ENABLE KEYS */;
-UNLOCK TABLES;
+insert  into `tblrole`(`id`,`name`) values (1,'Developer'),(2,'Administrator');
 
---
--- Table structure for table `tbluser`
---
+/*Table structure for table `tbluser` */
 
 DROP TABLE IF EXISTS `tbluser`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `tbluser` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `fname` varchar(50) NOT NULL,
@@ -153,25 +98,12 @@ CREATE TABLE `tbluser` (
   `secret` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `tbluser`
---
+/*Data for the table `tbluser` */
 
-LOCK TABLES `tbluser` WRITE;
-/*!40000 ALTER TABLE `tbluser` DISABLE KEYS */;
-INSERT INTO `tbluser` VALUES (1,'Chito','Alzona','Cascante','chito.cascante@gmail.com',1,'chito','chito123');
-/*!40000 ALTER TABLE `tbluser` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+insert  into `tbluser`(`id`,`fname`,`mname`,`lname`,`email`,`role_id`,`key`,`secret`) values (1,'Chito','Alzona','Cascante','chito.cascante@gmail.com',1,'chito','chito123');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2014-08-17 18:42:25
--- module/Customer/src/Customer/Model/Customer.php
+++ module/Customer/src/Customer/Model/Customer.php
@@ -5,28 +5,31 @@ namespace Customer\Model;
 class Customer
 {
 	public $id;
-	public $customer_category;
-	public $customer_type;
-	public $customer_name;
-	public $customer_consignee;
-	public $customer_branch;
-	public $customer_trade_name;
-	public $customer_tin;
-	public $customer_phone;
-	public $customer_email;
+	public $category_id;
+	public $type_id;
+	public $owner_name;
+	public $company_name;
+	public $consignee;
+	public $branch;
+	public $trade_name;
+	public $tin;
+	public $phone;
+	public $email;
+	public $assigned_se;
 	public $status;
 	
 	public function exchangeArray($data){
 		$this->id = (!empty($data['id'])) ? $data['id'] : null;
-		$this->customer_category = (!empty($data['customer_category'])) ? $data['customer_category'] : null;
-		$this->customer_type = (!empty($data['customer_type'])) ? $data['customer_type'] : null;
-		$this->customer_name= (!empty($data['customer_name'])) ? $data['customer_name'] : null;
-		$this->customer_consignee = (!empty($data['customer_consignee'])) ? $data['customer_consignee'] : null;
-		$this->customer_branch = (!empty($data['customer_branch'])) ? $data['customer_branch'] : null;
-		$this->customer_trade_name = (!empty($data['customer_trade_name'])) ? $data['customer_trade_name'] : null;
-		$this->customer_tin = (!empty($data['customer_tin'])) ? $data['customer_tin'] : null;
-		$this->customer_phone = (!empty($data['customer_phone'])) ? $data['customer_phone'] : null;
-		$this->customer_email = (!empty($data['customer_email'])) ? $data['customer_email'] : null;
+		$this->category_id = (!empty($data['category_id'])) ? $data['category_id'] : null;
+		$this->type_id = (!empty($data['type_id'])) ? $data['type_id'] : null;
+		$this->company_name= (!empty($data['company_name'])) ? $data['company_name'] : null;
+		$this->consignee = (!empty($data['consignee'])) ? $data['consignee'] : null;
+		$this->branch = (!empty($data['branch'])) ? $data['branch'] : null;
+		$this->trade_name = (!empty($data['trade_name'])) ? $data['trade_name'] : null;
+		$this->tin = (!empty($data['tin'])) ? $data['tin'] : null;
+		$this->phone = (!empty($data['phone'])) ? $data['phone'] : null;
+		$this->email = (!empty($data['email'])) ? $data['email'] : null;
+		$this->assigned_se = (!empty($data['assigned_se'])) ? $data['assigned_se'] : null;
 		$this->status = (!empty($data['status'])) ? $data['status'] : null;
 		
 	}
