3b4e1fd8f86c0cd5f5a4fc93d1035069cd8e4212
chito-sl <chito.cascante@gmail.com>
2014-08-21 14:01:17 -0700

    fix conflict in customer and product

--- module/Customer/src/Customer/Model/Customer.php
+++ module/Customer/src/Customer/Model/Customer.php
@@ -8,37 +8,6 @@ use Zend\InputFilter\InputFilterInterface;
 
 class Customer
 {
-<<<<<<< HEAD
-	public $id;
-	public $category_id;
-	public $type_id;
-	public $owner_name;
-	public $company_name;
-	public $consignee;
-	public $branch;
-	public $trade_name;
-	public $tin;
-	public $phone;
-	public $email;
-	public $assigned_se;
-	public $status;
-	
-	public function exchangeArray($data){
-		$this->id = (!empty($data['id'])) ? $data['id'] : null;
-		$this->category_id = (!empty($data['category_id'])) ? $data['category_id'] : null;
-		$this->type_id = (!empty($data['type_id'])) ? $data['type_id'] : null;
-		$this->company_name= (!empty($data['company_name'])) ? $data['company_name'] : null;
-		$this->consignee = (!empty($data['consignee'])) ? $data['consignee'] : null;
-		$this->branch = (!empty($data['branch'])) ? $data['branch'] : null;
-		$this->trade_name = (!empty($data['trade_name'])) ? $data['trade_name'] : null;
-		$this->tin = (!empty($data['tin'])) ? $data['tin'] : null;
-		$this->phone = (!empty($data['phone'])) ? $data['phone'] : null;
-		$this->email = (!empty($data['email'])) ? $data['email'] : null;
-		$this->assigned_se = (!empty($data['assigned_se'])) ? $data['assigned_se'] : null;
-		$this->status = (!empty($data['status'])) ? $data['status'] : null;
-		
-	}
-=======
     public $id;
     public $category;
     public $type;
@@ -275,5 +244,4 @@ class Customer
         }
         return $this->inputFilter;
     }
->>>>>>> 894633b52a3c520a683fb8632ed126953b7d0f06
 }
